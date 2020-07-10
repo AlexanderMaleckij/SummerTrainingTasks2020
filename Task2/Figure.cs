f2eb4b6c78ca027a7fc62fd62b615b22d3335e08
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-07-10 19:50:57 +0300

    Add  method "FindEqualsFigures" to Figure class

--- Task2/Figure.cs
+++ Task2/Figure.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace Task2Figures
@@ -8,6 +9,26 @@ namespace Task2Figures
         public abstract double Area();
         public abstract double Perimeter();
 
+        /// <summary>
+        /// looks in the array for all shapes equal to a given
+        /// </summary>
+        /// <param name="desiredShape">searching shape</param>
+        /// <param name="shapes">figures where to look</param>
+        /// <returns>shapes equal to a desiredShape</returns>
+        public static Figure[] FindEqualsFigures(Figure desiredShape, Figure[] shapes)
+        {
+            List<Figure> desiredFigures = new List<Figure>();
+
+            for(int i = 0; i < shapes.Length; i++)
+            {
+                if (desiredShape.Equals(shapes[i]))
+                {
+                    desiredFigures.Add(shapes[i]);
+                }
+            }
+
+            return desiredFigures.ToArray();
+        }
         /// <summary>
         /// Calculate distance between points
         /// </summary>
