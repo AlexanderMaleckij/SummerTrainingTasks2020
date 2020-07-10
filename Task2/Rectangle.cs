1471fda3b79867516b9b36e4dbc3b93c8a78dea0
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-07-10 13:47:55 +0300

    Added part of the Square and Rectangle classes  logic

--- Task2/Rectangle.cs
+++ Task2/Rectangle.cs
@@ -1,19 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Drawing;
 
 namespace Task2
 {
     class Rectangle : Figure
     {
-        public override double Area()
+        private double side1Length;
+        private double side2Length;
+
+        public double Side1Length
         {
-            throw new NotImplementedException();
+            get => side1Length;
+            set
+            {
+                if(value > 0)
+                {
+                    side1Length = value;
+                }
+            }
         }
 
-        public override double Perimeter()
+        public double Side2Length
         {
-            throw new NotImplementedException();
+            get => side2Length;
+            set
+            {
+                if (value > 0)
+                {
+                    side2Length = value;
+                }
+            }
         }
+
+        public Rectangle(double a, double b)
+        {
+
+        }
+
+        public Rectangle(PointF p1, PointF p2, PointF p3, PointF p4)
+        {
+            if(IsRectangle(p1, p2, p3, p4, out double side1Length, out double side2Length))
+            {
+                Side1Length = side1Length;
+                Side2Length = side2Length;
+            }
+        }
+
+        private bool IsRectangle(PointF p1, PointF p2, PointF p3, PointF p4, out double a, out double b)
+        {
+
+        }
+
+        public override double Area() => Side1Length * Side2Length;
+
+        public override double Perimeter() => 2 * (Side1Length + Side2Length);
     }
 }
--- Task2/Square.cs
+++ Task2/Square.cs
@@ -1,19 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Drawing;
 
 namespace Task2
 {
     class Square : Figure
     {
-        public override double Area()
+        double sideLength;
+
+        public double SideLength
         {
-            throw new NotImplementedException();
+            get => sideLength;
+            set
+            {
+                if (value > 0)
+                {
+                    sideLength = value;
+                }
+            }
         }
 
-        public override double Perimeter()
+        public Square(double sideLength)
         {
-            throw new NotImplementedException();
+            SideLength = sideLength;
         }
+
+        public Square(PointF p1, PointF p2, PointF p3, PointF p4)
+        {
+            if(IsSquare(p1, p2, p3, p4, out double sideLength))
+            {
+                SideLength = sideLength;
+            }
+        }
+
+        public bool IsSquare(PointF p1, PointF p2, PointF p3, PointF p4, out double sideLength)
+        {
+
+        }
+
+        public override double Area() => Math.Pow(SideLength, 2);
+
+        public override double Perimeter() => 4 * SideLength;
     }
 }
