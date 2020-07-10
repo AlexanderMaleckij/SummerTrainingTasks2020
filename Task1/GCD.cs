bb22034e0624ef7583a1f060e5a61d4437b62ef0
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-07-10 12:08:06 +0300

    Add overloaded methods for calulating GCD using Euclid's method

--- Task1/GCD.cs
+++ Task1/GCD.cs
@@ -29,7 +29,33 @@ namespace Task1
             while (divRemainder != 0);
             return b;
         }
-        
+
+        /// <summary>
+        /// Calculates greatest common divisor (GCD) of three numbers
+        /// </summary>
+        /// <param name="a">1st number</param>
+        /// <param name="b">2ns number</param>
+        /// <param name="c">3rd number</param>
+        /// <returns></returns>
+        public static int EuclidGCD(int a, int b, int c) => EuclidGCD(EuclidGCD(a, b), c);
+
+        /// <summary>
+        /// Calculates greatest common divisor (GCD) of four numbers
+        /// </summary>
+        /// <param name="a">1st number</param>
+        /// <param name="b">2nd number</param>
+        /// <param name="c">3rd number</param>
+        /// <param name="d">4th number</param>
+        /// <returns></returns>
+        public static int EuclidGCD(int a, int b, int c, int d) => EuclidGCD(EuclidGCD(a, b, c), d);
+
+
+        /// <summary>
+        /// Swaps the values of two variables
+        /// </summary>
+        /// <typeparam name="T">type of swapping values</typeparam>
+        /// <param name="a">1st variable for swap</param>
+        /// <param name="b">2nd variable for swap</param>
         private static void Swap<T>(ref T a, ref T b)
         {
             T tmp = a;
