e9d8f3616bd5a328df5fb465c5a787007df189ff
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-07-12 13:35:52 +0300

    add - Polynom operation

--- Task2/Polynom.cs
+++ Task2/Polynom.cs
@@ -48,7 +48,31 @@ namespace Task2
 
         public static Polynom operator -(Polynom p1, Polynom p2)
         {
+            List<Monomial> m1Copy = new List<Monomial>(p1.monomials);
+            List<Monomial> m2Copy = new List<Monomial>(p2.monomials);
 
+            for (int i = 0; i < m1Copy.Count; i++)
+            {
+                for (int j = 0; j < m2Copy.Count; j++)
+                {
+                    Monomial m1 = m1Copy[i];
+                    Monomial m2 = m2Copy[j];
+
+                    if (m1.xPower == m2.xPower)
+                    {
+                        m1.coefficient -= m2.coefficient;
+                        m2Copy.Remove(m2);
+                        j--;
+                    }
+
+                    if (m1.coefficient == 0)
+                    {
+                        m1Copy.Remove(m1);
+                        i--;
+                    }
+                }
+            }
+            return new Polynom(m1Copy);
         }
         public static Polynom operator *(Polynom p1, Polynom p2)
         {
