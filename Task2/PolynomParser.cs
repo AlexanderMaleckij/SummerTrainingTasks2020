8badf44a0ebe19aae2569c5ebc6c880552cffdd5
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-07-12 19:26:12 +0300

    Added polynom validating

--- Task2/PolynomParser.cs
+++ Task2/PolynomParser.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Task2
 {
@@ -8,7 +10,63 @@ namespace Task2
     {
         public static List<Monomial> Parse(string polynom)
         {
+            ValidatePolynom(polynom);
 
+
+
+           
+
+        }
+
+        private static void ValidatePolynom(string polynom)
+        {
+            RemoveExtremeBrackets(polynom);
+            CheckBrackets(polynom);
+            CheckPolynomFormat(polynom);
+            CheckForMultiplePolynomials(polynom);
+        }
+
+        private static void RemoveExtremeBrackets(string polynom)
+        {
+            polynom = polynom.Trim();
+
+            if (polynom.StartsWith("(") && polynom.EndsWith(")"))
+            {
+                polynom = polynom.Substring(1, polynom.Length - 2);
+            }
+        }
+
+        private static void CheckBrackets(string polynom)
+        {
+            if (polynom.Where(x => x == '(').Count() != polynom.Where(x => x == ')').Count())
+            {
+                throw new ArgumentException("The number of opening and closing brackets doesn't match");
+            }
+        }
+
+        private static void CheckPolynomFormat(string polynom)
+        {
+            Regex regex = new Regex(@"[^x\d^+*\-\.\(\)]|[\(\)x]{2,}|^[^\dx]$|[+\-*\.,^]{2,}|\^x|\d\*[^x]|x\d|\dx|[+\^]$|\([\D]\)");
+            if (regex.IsMatch(polynom))
+            {
+                string errors = string.Empty;
+                foreach (Match match in regex.Matches(polynom))
+                {
+                    errors += '\n' + match.Value;
+                }
+                throw new ArgumentException($"Polynom format error: {errors}");
+            }
+        }
+
+        private static void CheckForMultiplePolynomials(string polynom)
+        {
+            foreach (Match match in new Regex(@"\((.+)\)").Matches(polynom))
+            {
+                if (match.Value.Contains('('))
+                {
+                    throw new ArgumentException("Multiple polynomials found in a given line");
+                }
+            }
         }
     }
 }
