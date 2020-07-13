6d7b7dd75d53afa37f22091d24dccba0ccbad8dd
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-07-13 23:25:38 +0300

    Added Vector class comments

--- Task1/Vector.cs
+++ Task1/Vector.cs
@@ -2,6 +2,9 @@
 
 namespace Task1
 {
+    /// <summary>
+    /// class for working with three-dimensional vectors
+    /// </summary>
     public class Vector //http://netlib.narod.ru/library/book0032/part1_01.htm
     {
         private double x, y, z;
@@ -13,10 +16,26 @@ namespace Task1
             this.z = z;
         }
 
+        /// <summary>
+        /// Get vector length in space (module)
+        /// </summary>
+        /// <returns>module</returns>
         public double Module() => Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
 
+        /// <summary>
+        /// Operation of multiplying a vector by a number
+        /// </summary>
+        /// <param name="v">vector</param>
+        /// <param name="number">number</param>
+        /// <returns>multiplied vector</returns>
         public static Vector operator *(Vector v, double number) => new Vector(v.x * number, v.y * number, v.z * number);
 
+        /// <summary>
+        /// Operation of dividing a vector by a number
+        /// </summary>
+        /// <param name="v">vector</param>
+        /// <param name="number">number</param>
+        /// <returns>divided vector</returns>
         public static Vector operator /(Vector v, double number)
         { 
             if(number == 0)
@@ -27,10 +46,28 @@ namespace Task1
             return new Vector(v.x / number, v.y / number, v.z / number); 
         }
 
+        /// <summary>
+        /// Operation of adding two vectors
+        /// </summary>
+        /// <param name="u">1st vector</param>
+        /// <param name="v">2nd vector</param>
+        /// <returns>sum of vectors</returns>
         public static Vector operator +(Vector u, Vector v) => new Vector(u.x + v.x, u.y + v.y, u.z + v.z);
 
+        /// <summary>
+        /// Subtraction operation of two vectors
+        /// </summary>
+        /// <param name="u">1st vector</param>
+        /// <param name="v">2nd vector</param>
+        /// <returns>vector difference</returns>
         public static Vector operator -(Vector u, Vector v) => new Vector(u.x - v.x, u.y - v.y, u.z - v.z);
 
+        /// <summary>
+        /// collinearity check of two vectors
+        /// </summary>
+        /// <param name="u">1st vector</param>
+        /// <param name="v">2nd vector</param>
+        /// <returns>is collinear</returns>
         public static bool operator ==(Vector u, Vector v)
         {
             Vector vectorMul = VectorMultiplication(u, v);
@@ -45,6 +82,12 @@ namespace Task1
             }
         }
 
+        /// <summary>
+        /// non collinearity check of two vectors
+        /// </summary>
+        /// <param name="u">1st vector</param>
+        /// <param name="v">2nd vector</param>
+        /// <returns>is not collinear</returns>
         public static bool operator !=(Vector u, Vector v)
         {
             if(u == v)
@@ -57,10 +100,27 @@ namespace Task1
             }
         }
 
+        /// <summary>
+        /// vector sign change operation
+        /// </summary>
+        /// <param name="v">vector</param>
+        /// <returns>vector with changed sign</returns>
         public static Vector operator -(Vector v) => new Vector(-v.x, -v.y, -v.z);
 
+        /// <summary>
+        /// calculate scalar multiplication of 2 vectors
+        /// </summary>
+        /// <param name="u">1st vector</param>
+        /// <param name="v">2nd vector</param>
+        /// <returns>scalar multiplication of 2 vectors</returns>
         public static Vector ScalarMultiplication(Vector u, Vector v) => new Vector(u.x * v.x, u.y * v.y, u.z * v.z);
 
+        /// <summary>
+        /// calculate vector multiplication of 2 vectors
+        /// </summary>
+        /// <param name="u">1st vector</param>
+        /// <param name="v">2nd vector</param>
+        /// <returns>vector multiplication of 2 vectors</returns>
         public static Vector VectorMultiplication(Vector u, Vector v)
         {
             double x = u.y * v.z - u.z * v.y;
@@ -70,13 +130,26 @@ namespace Task1
             return new Vector(x, y, z);
         }
 
+        /// <summary>
+        /// Get string representation of the Vector class instance
+        /// </summary>
+        /// <returns>string representation of a class instance</returns>
         public override string ToString() => $"Vector {x} {y} {z}";
 
+        /// <summary>
+        /// Serves as a default hash function
+        /// </summary>
+        /// <returns>instance hash code</returns>
         public override int GetHashCode()
         {
             return (x.GetHashCode() << 2) ^ (y.GetHashCode() << 1) ^ z.GetHashCode();
         }
 
+        /// <summary>
+        /// Determines whether two instances of an object are equal
+        /// </summary>
+        /// <param name="obj">2nd instance for comparsion</param>
+        /// <returns>is equals</returns>
         public override bool Equals(object obj)
         {
             if(obj != null && obj is Vector)
