91ae92cb9ffad7bcce2696e00f9ab0d9c8ef8b18
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-07-14 13:17:36 +0300

    Added Product class

---
+++ Task3/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task3
+{
+    public abstract class Product
+    {
+        private double price;
+        private string name;
+
+        double Price
+        {
+            get => price;
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Price can't be negative");
+                }
+                else
+                {
+                    price = Math.Round(value, 2);
+                }
+            }
+        }
+        string Name
+        {
+            get => name;
+            set
+            {
+                if(!string.IsNullOrEmpty(name))
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Name of product must contain at least 1 symbol");
+                }
+            }
+        }
+
+        public static implicit operator double(Product product)
+        {
+            return product.price;
+        }
+
+        public static implicit operator int(Product product)
+        {
+            return (int)(product.price * 100);
+        }
+    }
+}
