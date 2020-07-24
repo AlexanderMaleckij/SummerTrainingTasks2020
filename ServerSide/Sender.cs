472b11d6c1f817cef43a896b6478d294a39ec954
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-07-24 13:45:40 +0300

    Added Sender class

---
+++ ServerSide/Sender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerSide
+{
+    public class Sender
+    {
+        public string Login { get; set; }
+        public Socket Socket { get; private set; }
+
+        public Sender(string login, Socket socket)
+        {
+            Login = login;
+            Socket = socket;
+        }
+    }
+}
---
+++ ServerSide/Server.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ServerSide
+{
+    public delegate void ClientMessageHandler(Sender sender, string message);
+    public class Server
+    {
+    }
+}
