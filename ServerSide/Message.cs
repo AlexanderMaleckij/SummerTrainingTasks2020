1b50a3fc5623b5420b7eb04efccc72da0d14ec9c
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-07-24 14:36:24 +0300

    Added Message class

---
+++ ServerSide/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerSide
+{
+    public class Message
+    {
+        public string MessageText { get; private set; }
+        public DateTime MessageDateTime { get; private set; }
+
+        public Message(string message)
+        {
+            MessageText = message;
+            MessageDateTime = DateTime.UtcNow;
+        }
+    }
+}
