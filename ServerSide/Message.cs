17d2d4dfb61b64578b15c9a5faab355a13517963
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-07-24 18:35:25 +0300

    Added Message class comments

--- ServerSide/Message.cs
+++ ServerSide/Message.cs
@@ -1,14 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ServerSide
 {
+    /// <summary>
+    /// Represent a message, that contains text and date
+    /// </summary>
     public class Message
     {
         public string MessageText { get; private set; }
         public DateTime MessageDateTime { get; private set; }
 
+        /// <summary>
+        /// Create a Message class instance with specified 
+        /// text of message and current date and time 
+        /// </summary>
+        /// <param name="message">Message class instance</param>
         public Message(string message)
         {
             MessageText = message;
