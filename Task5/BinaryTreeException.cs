9fbe77b6dee7ed395057215e6adc7391620d2889
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-07-27 18:40:13 +0300

    Added BinaryTreeException class

---
+++ Task5/BinaryTreeException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tree
+{
+    public class BinaryTreeException : Exception
+    {
+        public BinaryTreeException()
+        {
+        }
+
+        public BinaryTreeException(string message)
+            : base(message)
+        {
+        }
+
+        public BinaryTreeException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
