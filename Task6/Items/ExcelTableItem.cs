d38dbaa019ac5baed514815b260f3288765c84e0
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-08-20 18:57:49 +0300

    Added ExcelTextItem and ExcelTableItem classes

---
+++ Task6/Items/ExcelTableItem.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Data;
+
+namespace Excel.Items
+{
+    public class ExcelTableItem : ExcelItem
+    {
+        public System.Data.DataTable Table
+        {
+            get => Table;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ExcelItemException("Table can't be null");
+                }
+
+                Table = value;
+            }
+        }
+
+        public ExcelTableItem(System.Data.DataTable table)
+        {
+            this.Table = table;
+            Position = new ExcelItemPosition();
+        }
+
+        public ExcelTableItem(System.Data.DataTable table, ExcelItemPosition position)
+        {
+            Table = table;
+            Position = position;
+        }
+
+        public override void AddItem(Worksheet worksheet)
+        {
+            MarkupTablePlace(worksheet);
+            FillTableHeader(worksheet);
+            FillTableBody(worksheet);
+        }
+
+        private void MarkupTablePlace(Worksheet worksheet)
+        {
+            Range leftUpCorner = (Range)worksheet.Cells[Position.CellCoordNumberX,
+                                                        Position.CellCoordNumberY];
+
+            Range rightDownCorner = (Range)worksheet.Cells[Position.CellCoordNumberX + Table.Columns.Count,
+                                                           Position.CellCoordNumberY + Table.Rows.Count + 1];
+
+            Range tableRange = worksheet.get_Range(leftUpCorner, rightDownCorner);
+
+            worksheet.ListObjects.Add(
+                XlListObjectSourceType.xlSrcRange,
+                tableRange,
+                Type.Missing,
+                XlYesNoGuess.xlYes);
+        }
+
+        private void FillTableHeader(Worksheet worksheet)
+        {
+            int currentCellCoordX = Position.CellCoordNumberX;
+
+            foreach (DataColumn column in Table.Columns)
+            {
+                worksheet.Cells[currentCellCoordX, Position.CellCoordNumberY] = column.ColumnName;
+                currentCellCoordX++;
+            }
+        }
+
+        private void FillTableBody(Worksheet worksheet)
+        {
+            int xStart = Position.CellCoordNumberX;
+            int currentCellCoordY = Position.CellCoordNumberY + 1;
+
+            foreach (DataRow row in Table.Rows)
+            {
+                var cells = row.ItemArray;
+                int currentCellCoordX = xStart;
+
+                foreach (object cell in cells)
+                {
+                    worksheet.Cells[currentCellCoordX, currentCellCoordY] = cell;
+                    currentCellCoordX++;
+                }
+
+                currentCellCoordY++;
+            }
+        }
+    }
+}
---
+++ Task6/Items/ExcelTextItem.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+namespace Excel.Items
+{
+    public class ExcelTextItem : ExcelItem
+    {
+        private string text;
+        private ExcelItemSize size = new ExcelItemSize();
+        private ExcelStyler styler = new ExcelStyler();
+        public ExcelItemSize Size
+        {
+            get => size;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ExcelItemException("Excel item size can't be null");
+                }
+
+                size = value;
+            }
+        }
+        public ExcelStyler Styler
+        {
+            get => styler;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ExcelItemException("Style can't be null");
+                }
+
+                styler = value;
+            }
+        }
+        public string Text
+        {
+            get => text;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ExcelItemException("Text cant be null or empty");
+                }
+
+                text = value;
+            }
+        }
+        public ExcelTextItem(string text)
+        {
+            Text = text;
+        }
+
+        public override void AddItem(Worksheet worksheet)
+        {
+            worksheet.Cells[Position.CellCoordNumberY, Position.CellCoordNumberX] = Text;
+            styler.ApplyStyle((Range)worksheet.Cells[Position.CellCoordNumberY, Position.CellCoordNumberX]);
+
+            Range leftUpCorner = (Range)worksheet.Cells[Position.CellCoordNumberY,
+                                                       Position.CellCoordNumberX];
+
+            Range rightDownCorner = (Range)worksheet.Cells[Position.CellCoordNumberY + size.Height - 1,
+                                                          Position.CellCoordNumberX + size.Width - 1];
+
+            worksheet.get_Range(leftUpCorner, rightDownCorner).Merge();
+        }
+    }
+}
