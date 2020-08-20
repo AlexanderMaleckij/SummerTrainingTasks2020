70e463598e970a9ea9627f0307d9be9e0b1072c4
Alexander Maletski <sash.maleczckij201334@gmail.com>
2020-08-20 18:50:35 +0300

    Addded ExcelItem class

---
+++ Task6/Items/ExcelItem.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+namespace Excel.Items
+{
+    public abstract class ExcelItem
+    {
+        private ExcelItemPosition position = new ExcelItemPosition();
+
+        public ExcelItemPosition Position
+        {
+            get => position;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ExcelItemException("Excel item position can't be null");
+                }
+
+                position = value;
+            }
+        }
+
+        public abstract void AddItem(Worksheet worksheet);
+    }
+}
